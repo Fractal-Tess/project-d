ef627fa7f44821ff37aadd868b178f3904fac128
Fractal-Tess <vgfractal@gmail.com>
2024-06-28 09:49:22 +0300

    make it buildable

--- apps/anotator/next.config.js
+++ apps/anotator/next.config.js
@@ -6,7 +6,7 @@ await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
-  transpilePackages: ['@repo/ui'],
+  transpilePackages: ['@repo/ui','@repo/drizzle'],
   experimental: {
     serverComponentsExternalPackages: ['@node-rs/argon2']
   }
