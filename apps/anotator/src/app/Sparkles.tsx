6ffacdb0243d5b2d006cf15ae9a9048c991b6349
Fractal-Tess <vgfractal@gmail.com>
2024-06-28 09:26:40 +0300

    formatting & tsc added to lint

--- apps/anotator/src/app/Sparkles.tsx
+++ apps/anotator/src/app/Sparkles.tsx
@@ -1,22 +1,24 @@
-"use client";
-import { SparklesCore } from "@ui/components/Sparkles.client";
-import React from "react";
+'use client';
+import { SparklesCore } from '@ui/components/Sparkles.client';
+import React from 'react';
 
 export function Sparkles() {
   return (
-    <div className="h-screen w-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <div className="w-full absolute inset-0 h-screen">
+    <div className="relative flex h-screen w-full w-screen flex-col items-center justify-center overflow-hidden rounded-md bg-black">
+      <div className="absolute inset-0 h-screen w-full">
         <SparklesCore
           id="tsparticlesfullpage"
           background="transparent"
           minSize={0.6}
           maxSize={1.4}
           particleDensity={100}
-          className="w-full h-full"
+          className="h-full w-full"
           particleColor="#FFFFFF"
         />
       </div>
-      <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">Landing page</h1>
+      <h1 className="relative z-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-6xl">
+        Landing page
+      </h1>
     </div>
   );
 }
--- apps/anotator/src/app/page.tsx
+++ apps/anotator/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Sparkles } from './Sparkles'
+import { Sparkles } from './Sparkles';
 export default async function Home() {
   return (
     <section className="container mx-auto flex min-h-screen flex-col items-center justify-center gap-y-4">
